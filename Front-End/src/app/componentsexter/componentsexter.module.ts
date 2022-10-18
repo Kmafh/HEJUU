6e6b2445b2f1ee235147875d52a241c9fa2fe27d
Kmafh <artadapt@gmail.com>
2022-10-18 09:24:24 +0200

    Error en app.model corregido.

--- Front-End/src/app/componentsexter/componentsexter.module.ts
+++ Front-End/src/app/componentsexter/componentsexter.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OptionNadvarComponent } from './option-nadvar/option-nadvar.component';
-import { IngresosComponent } from './option-nadvar/ingresos.component';
 import { GastosComponent } from './optionNadvar/gastos/gastos.component';
+import { IngresosComponent } from './optionNadvar/ingresos/ingresos.component';
 
 
 
 @NgModule({
   declarations: [
-    OptionNadvarComponent,
     IngresosComponent,
     GastosComponent
   ],
